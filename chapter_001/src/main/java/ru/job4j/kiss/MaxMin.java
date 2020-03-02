94f81d3acf15bdd6ec183710045e0e191ddde7b3
Roux13 <u.nehodov@gmail.com>
2020-03-02 17:13:11 +0300

    Часть OOD. 0. TDD / 1. Принципы Kiss, Dry и Yagni[#243307]. Сортировка заменена на max/min

--- chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,21 +1,15 @@
 package ru.job4j.kiss;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        int index = value.size() - 1;
-        return sortAndGetNumber(value, comparator, index);
+        return Collections.max(value, comparator);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        int index = 0;
-        return sortAndGetNumber(value, comparator, index);
-    }
-
-    private <T> T sortAndGetNumber(List<T> value, Comparator<T> comparator, int index) {
-        value.sort(comparator);
-        return value.get(index);
+        return Collections.min(value, comparator);
     }
 }
